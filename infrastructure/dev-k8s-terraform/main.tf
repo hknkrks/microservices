cf20191532509b99b7ba25d0a419a8a793231afa
hknkrks <hakankarakasmail@gmail.com>
2022-09-17 08:41:46 +0300

    ami change

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -133,8 +133,8 @@ resource "aws_security_group" "matt-kube-master-sg" {
 }
 
 resource "aws_instance" "kube-master" {
-    ami = "ami-013f17f36f8b1fefb"
-    instance_type = "t3a.medium"
+    ami = "ami-0888c389af05d881a"
+    instance_type = "t2.micro"
     iam_instance_profile = module.iam.master_profile_name
     vpc_security_group_ids = [aws_security_group.matt-kube-master-sg.id, aws_security_group.matt-kube-mutual-sg.id]
     key_name = "mattkey"
@@ -151,7 +151,7 @@ resource "aws_instance" "kube-master" {
 }
 
 resource "aws_instance" "worker-1" {
-    ami = "ami-013f17f36f8b1fefb"
+    ami = "ami-0888c389af05d881a"
     instance_type = "t3a.medium"
         iam_instance_profile = module.iam.worker_profile_name
     vpc_security_group_ids = [aws_security_group.matt-kube-worker-sg.id, aws_security_group.matt-kube-mutual-sg.id]
@@ -169,7 +169,7 @@ resource "aws_instance" "worker-1" {
 }
 
 resource "aws_instance" "worker-2" {
-    ami = "ami-013f17f36f8b1fefb"
+    ami = "ami-0888c389af05d881a"
     instance_type = "t2.medium"
     iam_instance_profile = module.iam.worker_profile_name
     vpc_security_group_ids = [aws_security_group.matt-kube-worker-sg.id, aws_security_group.matt-kube-mutual-sg.id]
